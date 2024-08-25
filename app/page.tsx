e90e643de68fbaf68111a54ec3399853b4398157
Diego Prado <diegoprd@gmail.com>
2024-08-25 12:19:27 +0200

    fixing the full screen

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,51 @@
 "use client";
 
+import React, { useState } from "react";
 import {
   InformationCircleIcon,
   UserPlusIcon,
   QuestionMarkCircleIcon,
 } from "@heroicons/react/24/outline";
 import Link from "next/link";
-import { useEffect } from "react";
-import Button from "./components/button";
+import Button from "./components/button"; // Ensure this path is correct
 
 export default function Home() {
-  useEffect(() => {
-    // Disable context menus across the entire app
-    // const handleContextMenu = (event: MouseEvent) => {
-    //   event.preventDefault();
-    // };
+  // State to manage whether the fullscreen button is shown
+  const [showFullScreenButton, setShowFullScreenButton] = useState(true);
 
-    // document.addEventListener("contextmenu", handleContextMenu);
+  const handleFullScreen = () => {
+    if (document.documentElement.requestFullscreen) {
+      document.documentElement.requestFullscreen().catch((e) => {
+        console.log("Error attempting to enable full-screen mode:", e);
+      });
+      // Hide the button after requesting fullscreen
+      setShowFullScreenButton(false);
+    }
+  };
 
-    document?.documentElement?.requestFullscreen().catch((e) => {
-      console.log("Error attempting to enable full-screen mode:", e);
-    });
+  // useEffect(() => {
+  // Disable context menus across the entire app
+  // const handleContextMenu = (event: MouseEvent) => {
+  //   event.preventDefault();
+  // };
 
-    // return () => {
-    //   document.removeEventListener("contextmenu", handleContextMenu);
-    // };
-  }, []);
+  // document.addEventListener("contextmenu", handleContextMenu);
+
+  // return () => {
+  //   document.removeEventListener("contextmenu", handleContextMenu);
+  // };
+  // }, []);
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-hero-pattern bg-cover bg-center ">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-hero-pattern bg-cover bg-center">
+      {showFullScreenButton && (
+        <button
+          onClick={handleFullScreen}
+          className="mb-4 p-2 bg-blue-500 text-white font-bold rounded hover:bg-blue-700 transition duration-150 ease-in-out"
+        >
+          Enter Fullscreen
+        </button>
+      )}
       <div className="flex justify-start items-end space-x-6">
         <Button
           text="Como Funciona"
@@ -43,7 +60,7 @@ export default function Home() {
         <Button
           text="Preguntas Frecuentes"
           heroIcon={QuestionMarkCircleIcon}
-          link="external-pages/faq"
+          link="/external-pages/faq"
         />
       </div>
     </div>

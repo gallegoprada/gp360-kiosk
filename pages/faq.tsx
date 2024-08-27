61260e856ede60d7f176a56f580b0385d135f858
Diego Prado <diegoprd@gmail.com>
2024-08-27 12:40:08 +0200

    removing all workaroudns

--- pages/faq.tsx
+++ pages/faq.tsx
@@ -10,85 +10,6 @@ interface FaqContainerProps {
   childContainerWidth: number;
 }
 
-interface IframeContainerProps {
-  childContainerHeight: number;
-  childContainerWidth: number;
-  onFinishLoading: any;
-}
-
-const IframeContainer: React.FC<IframeContainerProps> = ({
-  childContainerHeight,
-  childContainerWidth,
-  onFinishLoading,
-}) => {
-  useEffect(() => {
-    const iframeWrapper = document.querySelector(".iframe-wrapper");
-
-    if (iframeWrapper) {
-      let startX = 0;
-      let startY = 0;
-
-      // Use React.TouchEvent<HTMLDivElement> if handling events on a div
-      const handleTouchStart = (e: React.TouchEvent<HTMLDivElement>) => {
-        startX = e.touches[0].pageX;
-        startY = e.touches[0].pageY;
-      };
-
-      const handleTouchMove = (e: React.TouchEvent<HTMLDivElement>) => {
-        const moveX = e.touches[0].pageX;
-        const moveY = e.touches[0].pageY;
-
-        const diffX = Math.abs(moveX - startX);
-        const diffY = Math.abs(moveY - startY);
-
-        if (diffX > diffY) {
-          e.preventDefault();
-        }
-      };
-
-      if (iframeWrapper) {
-        iframeWrapper.addEventListener("touchstart", handleTouchStart as any, {
-          passive: false,
-        });
-        iframeWrapper.addEventListener("touchmove", handleTouchMove as any, {
-          passive: false,
-        });
-      }
-
-      return () => {
-        if (iframeWrapper) {
-          iframeWrapper.removeEventListener(
-            "touchstart",
-            handleTouchStart as any
-          );
-          iframeWrapper.removeEventListener(
-            "touchmove",
-            handleTouchMove as any
-          );
-        }
-      };
-    }
-  }, []);
-
-  return (
-    <iframe
-      id={"iframe-content"}
-      className="w-full h-full rounded-md"
-      style={{
-        overflowX: "hidden",
-        touchAction: "pan-y",
-        pointerEvents: "none",
-      }}
-      height={childContainerHeight}
-      width={childContainerWidth}
-      src="https://gallegoprada360.com/faq/"
-      frameBorder="0"
-      allowFullScreen
-      onLoad={() => onFinishLoading()}
-    ></iframe>
-  );
-};
-
 const FaqContainer: React.FC<FaqContainerProps> = ({
   childContainerHeight,
   childContainerWidth,
@@ -106,7 +27,6 @@ const FaqContainer: React.FC<FaqContainerProps> = ({
     >
       {/* <IdleTimerWrapper> */}
       <div
-        id={"iframe-wrapper"}
         style={{
           position: "relative",
           height: childContainerHeight,
@@ -115,18 +35,26 @@ const FaqContainer: React.FC<FaqContainerProps> = ({
           touchAction: "pan-y",
         }}
       >
-        {/* {isLoading && (
+        {isLoading && (
           <div className="w-full h-full rounded-md flex flex-col justify-center items-center block p-6 text-gray-900 dark:text-white bg-white border border-gray-200 rounded-lg shadow  dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 opacity-80 ">
             Loading...
           </div>
-        )} */}
-        {/*  */}
+        )}
 
-        <IframeContainer
-          childContainerHeight={childContainerHeight}
-          childContainerWidth={childContainerWidth}
-          onFinishLoading={() => setIsLoading(false)}
-        />
+        <iframe
+          id={"iframe-content"}
+          className="w-full h-full rounded-md"
+          style={{
+            overflowX: "hidden",
+            touchAction: "pan-y",
+          }}
+          height={childContainerHeight}
+          width={childContainerWidth}
+          src="https://gallegoprada360.com/faq/"
+          frameBorder="0"
+          allowFullScreen
+          onLoad={() => setIsLoading(false)}
+        ></iframe>
       </div>
       {/* </IdleTimerWrapper> */}
     </div>

e718d0dff06de9b781225c2d5f888ba3e4f68fc2
Diego Prado <diegoprd@gmail.com>
2024-08-28 11:02:20 +0200

    using bakground differently

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,8 @@ const config: Config = {
   theme: {
     extend: {
       backgroundImage: {
-        "hero-pattern":
-          "url('https://gallegoprada360.com/wp-content/uploads/2020/09/fde1daa2-f639-408a-b603-bd9b30a24c9e.webp')",
+        // "hero-pattern":
+        //   "url('https://gallegoprada360.com/wp-content/uploads/2020/09/fde1daa2-f639-408a-b603-bd9b30a24c9e.webp')",
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
